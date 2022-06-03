75c5b3b46f1ee808b6810ea2bf476c7a66b7ec26
kany west <luis-94@live.com>
2022-06-03 01:53:57 -0300

    agregando productos

--- public/main.js
+++ public/main.js
@@ -4,17 +4,15 @@ const enviarMensaje = () => {
     const author = document.getElementById("author").value;
     const text = document.getElementById("text").value;
     const time = new Date()
-    
 
     const mensaje = {
         author,
         text,
         time
-       
     };
     socket.emit('new_message', mensaje);
     return false;
-}
+};
 
 
 const crearEtiquetasMensaje = (mensaje) => {
@@ -22,24 +20,63 @@ const crearEtiquetasMensaje = (mensaje) => {
         author,
         text,
         time
-      
     } = mensaje;
+
     return `
-    
     <div>
-        <strong>${author}</strong>
-        <em>${text}</em>
-        <em>${time}</em>
-       
+        <strong style="color:blue">${author}  </strong>
+        <sm style="color:brown">${time}  :  </sm>
+        <em style="color:green">${text}    </em>
     </div>
     `;
-}
+};
 
+const sendProducts  = () =>{
+    const title = document.getElementById('title').value;
+    const price = document.getElementById('price').value;
+    const img = document.getElementById('img').value;
+
+    const product = {
+        title,
+        price,
+        img
+    };
+
+    socket.emit('new_products', product);
+    return false
+};
+
+const createProducts = (product) =>{
+    const {
+        title,
+        price,
+        img
+    } = product;
+    console.log(product);
+
+    return `
+    
+    <tr>
+    <td>${title}</td>
+    <td>${price}</td>
+    <td>
+        
+        <img src=${img} alt="imageName">
+    </td>
+    </tr>
+    `;
+}
 
 
 const agregarMensajes = (mensajes) => {
     const mensajesFinal = mensajes.map(mensaje => crearEtiquetasMensaje(mensaje)).join(" ");
     document.getElementById("messages").innerHTML = mensajesFinal;
-}
+};
+
+const addProducts = (products) => {
+    const totalProducts = products.map(product => createProducts(product)).join(" ");
+    document.getElementById("tableProducts").innerHTML = totalProducts;
+};
 
 socket.on('messages', (messages) => agregarMensajes(messages));
+socket.on('products', (products) => addProducts(products));
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express')
+const express = require('express');
 const handlebars = require('express-handlebars');
-const {Server:HttpServer}=require('http')
-const {Server: IOServer}=require('socket.io')
+const {Server:HttpServer}=require('http');
+const {Server: IOServer}=require('socket.io');
 
-const messages =[]
+const messages =[];
+const products =[];
 
-const app = express()
-const httpServer = new HttpServer(app)
-const ioServer = new IOServer(httpServer)
+const app = express();
+const httpServer = new HttpServer(app);
+const ioServer = new IOServer(httpServer);
 
-app.use(express.static('./public'))
+app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
@@ -24,7 +25,7 @@ app.engine(
 )
 
 app.set('view engine','hbs');
-app.set('views','./views')
+app.set('views','./views');
 
 app.get('/', (req, res) => {
     res.render('main');
@@ -33,13 +34,19 @@ app.get('/', (req, res) => {
 
 ioServer.on('connection',(socket)=>{
     
-    socket.emit('messages',messages)
+    socket.emit('messages',messages);
+    socket.emit('products',products);
     
     socket.on('new_message',(mensaje)=>{
-        messages.push(mensaje)
-        ioServer.sockets.emit('messages',messages)
-    })
+        messages.push(mensaje);
+        ioServer.sockets.emit('messages',messages);
+    });
+
+    socket.on('new_products',(produc)=>{
+        messages.push(produc);
+        ioServer.sockets.emit('productos',products);
+    });
     
 });
 
-httpServer.listen(8080, ()=>console.log("servidor corriendo en puerto 8080"))
+httpServer.listen(8080, ()=>console.log("servidor corriendo en puerto 8080"));
